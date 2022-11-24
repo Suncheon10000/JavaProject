e295c087e204ff76cc5bdf93bb1faa602d9c42cb
Kim gyeong su <sk62628504@gmail.com>
2022-11-24 20:56:32 +0900

    modify

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,13 @@ import java.io.File;
 
 public class Main extends JFrame {
     public Main() {
+        // 이미지
         Toolkit tk = Toolkit.getDefaultToolkit();
         Image YTB = tk.getImage("img/YTB.png");
         Image LSJ = tk.getImage("img/LSJ.png");
         Image PJH = tk.getImage("img/PJH.png");
 
+        // 이미지 출력
         JPanel pn = new JPanel(){
             @Override
             public void paint(Graphics g){
@@ -19,13 +21,13 @@ public class Main extends JFrame {
             }
 
         };
-
-        JLabel YTBjLabel = new JLabel("난이도 : 쉬움");
-        JLabel LSJjLabel = new JLabel("난이도 : 보통");
-        JLabel PJHjLabel = new JLabel("난이도 : 어려움");
+        // 선택하는 버튼
+        JPanel jpn = new JPanel();
         JButton YTBbutton = new JButton("선택");
         JButton LSJbutton = new JButton("선택");
         JButton PJHbutton = new JButton("선택");
+        
+        // Frame의 크기, 위치
         add("Center", pn);
         setTitle("똥 피하기");
         setBounds(0, 0, 1800, 1100);
@@ -33,18 +35,16 @@ public class Main extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        pn.add(YTBjLabel);
-        pn.add(LSJjLabel);
-        pn.add(PJHjLabel);
-        pn.add(YTBbutton);
-        pn.add(LSJbutton);
-        pn.add(PJHbutton);
+        // 버튼 추가
+        jpn.add(YTBbutton);
+        jpn.add(LSJbutton);
+        jpn.add(PJHbutton);
 
-        pn.setVisible(true);
-
-        YTBbutton.setBounds(0,0,130,75);
+        // 버튼의 좌표
+        YTBbutton.setBounds(0,0,100,75);
         LSJbutton.setBounds(600,1000,130,75);
         PJHbutton.setBounds(1100,1000,130,75);
+        jpn.setVisible(true);
 
     }
     public static void main(String[] args) {
