3bd881664cd64997850cf73b36a752e8476b17c1
Kim gyeong su <sk62628504@gmail.com>
2022-11-25 21:56:44 +0900

    게임 시작되면 스톱워치 출력

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,16 +1,27 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class GameFrame extends JFrame {
     private JLabel backgroundMap;
     private Player player;
+    int mm,ss, ms, t=0;
+    JButton start;
+    JLabel w1,w2,w3;
+    Thread p_display;
+
+    // 생성자
     GameFrame(){
         super("똥 피하기"); //타이틀
         JPanel jPanel = new JPanel();
 
         jPanel.setBackground(Color.GREEN);
+
+        // 시간 GUI
+        buildGUI();
 
         add(jPanel);
         Dimension frameSize = getSize();
@@ -24,12 +35,16 @@ public class GameFrame extends JFrame {
         initSetting();
         initListener();
     }
+    
+    // 배경화면
     private void initObject() {
         backgroundMap = new JLabel(new ImageIcon("image/backgroundMap.png"));
         setContentPane(backgroundMap);
         player = new Player();
         add(player);
     }
+    
+    // Frame 설정
     private void initSetting() {
         setSize(1600,900);
         setLayout(null);
@@ -37,6 +52,7 @@ public class GameFrame extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+    // 방향키 조절
     private void initListener() {
         addKeyListener(new KeyAdapter() {
             @Override
@@ -54,6 +70,94 @@ public class GameFrame extends JFrame {
             }
         });
     }
+
+
+    // 시간 스톱워치 화면 GUI
+    public void buildGUI() {
+        JPanel p = new JPanel(new BorderLayout());
+        JPanel bp = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JPanel wp = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        JLabel c1 = new JLabel(" : ");
+        JLabel c2 = new JLabel(" : ");
+        w1 = new JLabel("00");
+        w2 = new JLabel("00");
+        w3 = new JLabel("00");
+
+        JButton start = new JButton("START");
+        bp.add(start);
+        wp.add(w1);
+        wp.add(c1);
+        wp.add(w2);
+        wp.add(c2);
+        wp.add(w3);
+
+
+
+        start.setFont(new Font("serif",Font.BOLD,25));
+
+        w1.setFont(new Font("courier",Font.BOLD,30));
+        w2.setFont(new Font("courier",Font.BOLD,30));
+        w3.setFont(new Font("courier",Font.BOLD,30));
+
+        c1.setFont(new Font("courier",Font.BOLD,30));
+        c2.setFont(new Font("courier",Font.BOLD,30));
+        p.add(wp, BorderLayout.CENTER);
+        p.add(bp, BorderLayout.SOUTH);
+        p.setBounds(0,0,30,30);
+        add(p);
+        setVisible(true);
+        new ButtonListener();
+        System.out.println("-------------------게임 시작-------------------");
+
+    }
+
+    // start를 눌렀을때 시간이 자동으로 1초,2초,3초 ...
+    public class ButtonListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+
+            start.setEnabled(false);
+            p_display = new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+
+                    mm = Integer.parseInt(w1.getText());
+                    ss = Integer.parseInt(w2.getText());
+                    ms = Integer.parseInt(w3.getText());
+
+
+                    while (p_display == Thread.currentThread()) {
+
+                        mm =  t % (1000*60) / 100 / 60 ;
+                        ss = t % (1000*60) / 100 % 60 ;
+                        ms = t %100;
+
+                        try {
+                            Thread.sleep(10);
+
+                            w1.setText(String.format("%02d", mm));
+                            w2.setText(String.format("%02d", ss));
+                            w3.setText(String.format("%02d", ms));
+
+                            t++;
+
+                        }catch (InterruptedException e1) {}
+                    }
+                }
+            });
+
+
+            p_display.start();
+
+        }
+    }
+
+
+
 
 
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -8,21 +8,28 @@ public class MainFrame extends JFrame{
     Image YTB;
     Image LSJ;
     Image PJH;
+    
+    // 생성자
     MainFrame(){
         JFrame frame = new JFrame();
         JPanel imgPanel = new JPanel();
         JPanel StartPanel = new JPanel();
-        JLabel title = new JLabel();
+        JLabel title = new JLabel("금쪽이 피하기");
+        
+        // 게임타이틀 좌표
+        title.setSize(80,30);
+        title.setLocation(0,0);
+        add(title);
+
 
         // 선택하기 Button 생성
         JButton StartBtn = new JButton("GameStart");
-        add(StartBtn);
 
         // Button 좌표
         StartBtn.setBounds(1030, 700, 480, 90);
         StartBtn.setFont(new Font("Arial", Font.ITALIC, 80));
 
-
+        add(StartBtn);
 
         // 이미지 생성
         Toolkit toolkit = frame.getToolkit();
@@ -35,7 +42,6 @@ public class MainFrame extends JFrame{
             @Override
             public void actionPerformed(ActionEvent e) {
                 new GameFrame();
-                setVisible(false); // 창 안보이게 하기
             }
         });
 
@@ -45,12 +51,14 @@ public class MainFrame extends JFrame{
         setLayout(null);
         setVisible(true);
     }
+    
+    
+    // 이미지 좌표
     public void paint(Graphics g){
         g.drawImage(YTB,30,110,this);
         g.drawImage(LSJ,470,80,this);
         g.drawImage(PJH,970,130,this);
     }
-
 }
 
 
