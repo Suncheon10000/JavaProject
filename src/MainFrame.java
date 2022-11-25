1cfc8ec7f66539113c5f1cf1f22c1f69802a860a
Kim gyeong su <sk62628504@gmail.com>
2022-11-25 13:23:26 +0900

    start위치 조정

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -5,27 +5,31 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class MainFrame extends JFrame{
-
     Image YTB;
     Image LSJ;
     Image PJH;
     MainFrame(){
         JFrame frame = new JFrame();
+        JPanel imgPanel = new JPanel();
+        JPanel StartPanel = new JPanel();
+        JLabel title = new JLabel();
 
         // 선택하기 Button 생성
-        JButton StartBtn = new JButton("시작하기");
+        JButton StartBtn = new JButton("GameStart");
         add(StartBtn);
 
         // Button 좌표
-        StartBtn.setBounds(660, 770, 100, 30);
+        StartBtn.setBounds(1030, 700, 480, 90);
+        StartBtn.setFont(new Font("Arial", Font.ITALIC, 80));
+
 
 
         // 이미지 생성
-        Container c = getContentPane();
         Toolkit toolkit = frame.getToolkit();
         this.YTB = toolkit.getImage("img/YTB.png");
         this.LSJ = toolkit.getImage("img/LSJ.png");
         this.PJH = toolkit.getImage("img/PJH.png");
+
 
         StartBtn.addActionListener(new ActionListener() {
             @Override
@@ -34,8 +38,6 @@ public class MainFrame extends JFrame{
                 setVisible(false); // 창 안보이게 하기
             }
         });
-
-
 
         setTitle("똥 피하기");
         setSize(1600,900);
@@ -46,7 +48,7 @@ public class MainFrame extends JFrame{
     public void paint(Graphics g){
         g.drawImage(YTB,30,110,this);
         g.drawImage(LSJ,470,80,this);
-        g.drawImage(PJH,970,170,this);
+        g.drawImage(PJH,970,130,this);
     }
 
 }
